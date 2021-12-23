d157763713827ed90efec6b51a4dae467fc64ed3
francisconeto00 <franciscodgneto@gmail.com>
2021-12-23 15:32:40 +0000

    objetivo 1 completo

--- prog1.c
+++ prog1.c
@@ -1,15 +1,16 @@
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX 100000
 
 char buffer[1000];
 int nln =0;
 int nln1 =0;
-
 int readchar(int fd){
     char c[1];
     if(read(fd, c, 1) != 0){
@@ -28,16 +29,16 @@ ssize_t readln(int fd, char *line, size_t size){
     int ln=0;
 
     char c = readchar(fd);
-
     while(c > 0 && c != '\n' && i < size){
-        line[i] = c;
+        line[i] = c; 
+
         i++;
         c = readchar(fd);
         
         }
-    line[i] = 0;
-    
+       
    
+    line[i] = 0;
 
     return i;
 }
@@ -45,14 +46,14 @@ ssize_t readln(int fd, char *line, size_t size){
 
 
 
+
 int main(int argc, char* argv[]){
     char line[256];
      
-     int x = atoi(argv[2]);
+     int x = atoi(argv[1]);
 
-    
-    int fd = open("exemplo.txt", O_RDONLY); 
-    FILE *file=fopen("exemplo.txt","r");
+    int fd = open(argv[2], O_RDONLY); 
+    FILE *file=fopen(argv[2],"r");
     while(readln(fd, line, 200) > 0){
        nln1++;
         if(x>=nln1){
@@ -61,8 +62,19 @@ int main(int argc, char* argv[]){
     
 
     }
+    printf("-------------------\n");
+    char temp[512];
+    char *str="sistemas operativos";
+     lseek(fd,0,SEEK_SET);
+     int ret;
+      
+     while(fgets(temp,512,file)!=NULL){
+       if((strcasestr(temp,str))!=NULL){
+           printf("%s",temp);
+       }
+     }
   
-    
+    fseek(file,0,SEEK_SET);
     printf("-------------------\n");
     int i=0;
     while(fgets(line, sizeof line,file)!=NULL){
@@ -71,7 +83,7 @@ int main(int argc, char* argv[]){
     }
     
     int aux=i-x;
-     
+   
      lseek(fd,0,SEEK_SET);
     while(readln(fd, line, 200) > 0){
          nln++;
@@ -81,7 +93,10 @@ int main(int argc, char* argv[]){
           
             
     
-    }   
+    }
+       
    
     return 0;
+}
+
 }
